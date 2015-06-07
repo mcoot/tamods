41934e755705d8a4737614cda7a22bee1a9480d5
Todd Butler <Toddbutler93@outlook.com.au>
2015-06-07 15:58:17 +1000

    Added chat colors in comment form, needs work

--- TAMods/Mod_HudMod.cpp
+++ TAMods/Mod_HudMod.cpp
@@ -5,10 +5,46 @@ int _g = 0;
 int _b = 0;
 int _a = 255;
 
+//_chatCol[ChatID][R,G,B,A] Yourchat = 0, Enemy = 1, Team = 2
+//int _chatCol[3][4] = { { 255, 0, 255, 255 }, { 0, 255, 0, 255 }, { 0, 255, 255, 255 } };
+
 bool TrHUD_eventPostRender(int ID, UObject *dwCallingObject, UFunction* pFunction, void* pParams, void* pResult) {
+	
 	ATrHUD *that = (ATrHUD *)dwCallingObject;
-	// Create an FColor
+
+	//TODO Create config loading
+
+	/*
+	//Friend chat, enemy chat, team chat
+	FColor *FCColor = new FColor();
+	FColor *ECColor = new FColor();
+	FColor *TCColor = new FColor();
+
+	FCColor->R = _chatCol[0][0];
+	FCColor->G = _chatCol[0][1];
+	FCColor->B = _chatCol[0][2];
+	FCColor->A = _chatCol[0][3];
+
+	ECColor->R = _chatCol[1][0];
+	ECColor->G = _chatCol[1][1];
+	ECColor->B = _chatCol[1][2];
+	ECColor->A = _chatCol[1][3];
+
+	TCColor->R = _chatCol[2][0];
+	TCColor->G = _chatCol[2][1];
+	TCColor->B = _chatCol[2][2];
+	TCColor->A = _chatCol[2][3];
+
+	that->FriendlyChatColor = *FCColor;
+	that->EnemyChatColor = *ECColor;
+	that->TeamChatColor = *TCColor;
+	
+	*/
+
+	// Create an FColor, set Alpha from config
 	FColor *a = new FColor();
+	_a = g_config.damNumA;
+	a->A = _a;
 
 	if (g_config.showRainbow == true){
 		//TODO switch statement should be replaced with a while statement
@@ -57,11 +93,10 @@ bool TrHUD_eventPostRender(int ID, UObject *dwCallingObject, UFunction* pFunctio
 		}
 
 		//Apply settings
-		a->A = 255;
 		a->R = _r;
 		a->G = _g;
 		a->B = _b;
-
+		
 		that->m_OverheadNumberColorMax = *a;
 		that->m_OverheadNumberColorMin = *a;
 	}
@@ -71,9 +106,7 @@ bool TrHUD_eventPostRender(int ID, UObject *dwCallingObject, UFunction* pFunctio
 		_r = g_config.damNumR;
 		_g = g_config.damNumG;
 		_b = g_config.damNumB;
-		_a = g_config.damNumA;
 
-		a->A = _a;
 		a->R = _r;
 		a->G = _g;
 		a->B = _b;
