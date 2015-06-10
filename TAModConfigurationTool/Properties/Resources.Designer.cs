83d3da1fcfe94e6c2535e2975152b20410d06e52
mcoot <joe.spearritt@live.com>
2015-06-10 15:55:27 +1000

    Fixed another background error

--- TAModConfigurationTool/Properties/Resources.Designer.cs
+++ TAModConfigurationTool/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace TAModConfigurationTool.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap background {
-            get {
-                object obj = ResourceManager.GetObject("background", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap background_kata {
             get {
                 object obj = ResourceManager.GetObject("background_kata", resourceCulture);
