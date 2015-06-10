e9a6d57ba78d17a0736e447379240ca060005abe
mcoot <joe.spearritt@live.com>
2015-06-10 15:53:43 +1000

    Fixed config background

--- TAModConfigurationTool/MainForm.Designer.cs
+++ TAModConfigurationTool/MainForm.Designer.cs
@@ -2322,7 +2322,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImage = global::TAModConfigurationTool.Properties.Resources.background_kata;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.ClientSize = new System.Drawing.Size(944, 501);
             this.Controls.Add(this.tableLayoutPanel1);
--- TAModConfigurationTool/Properties/Resources.Designer.cs
+++ TAModConfigurationTool/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace TAModConfigurationTool.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap background_kata {
+            get {
+                object obj = ResourceManager.GetObject("background_kata", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
