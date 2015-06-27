6cbcd554e8c7f3ca32e92eb2e9e9ab6ee2e25d59
Ensis <ensis.tribes@gmail.com>
2015-06-27 19:23:54 +0200

    Allow modification of all projectiles (in theory, but the scripting API is not powerful enough yet to try it)

--- TAMods/Mod_Projectiles.cpp
+++ TAMods/Mod_Projectiles.cpp
@@ -2,28 +2,24 @@
 
 static CustomProjectile *fired_proj = NULL;
 
-bool TrDev_ConstFire_WeaponConstantFiring(int ID, UObject *dwCallingObject, UFunction* pFunction, void* pParams, void* pResult)
+bool TrDev_WeaponFiring(int ID, UObject *dwCallingObject, UFunction* pFunction, void* pParams, void* pResult)
 {
 	ATrDevice *that = (ATrDevice *)dwCallingObject;
 
 	if (that) {
 		if (that->ShouldRefire()) {
-			if (that->MaxAmmoCount > 10) {
-				//ATrProjectile *default_proj;
-
-				// Retrieve default object for the projectile that will be fired
-				auto it = g_config.wep_id_to_custom_proj.find(that->DBWeaponId);
-				if (it == g_config.wep_id_to_custom_proj.end() || !it->second)
-					return false;
-				fired_proj = it->second;
-				fired_proj->default_proj->ProjFlightTemplate = fired_proj->custom_ps;
-			}
+			// Retrieve default object for the projectile that will be fired
+			auto it = g_config.wep_id_to_custom_proj.find(that->DBWeaponId);
+			if (it == g_config.wep_id_to_custom_proj.end() || !it->second)
+				return false;
+			fired_proj = it->second;
+			fired_proj->default_proj->ProjFlightTemplate = fired_proj->custom_ps;
 		}
 	}
 	return false;
 }
 
-bool TrDev_ConstFire_WeaponConstantFiring_POST(int ID, UObject *dwCallingObject, UFunction* pFunction, void* pParams, void* pResult)
+bool TrDev_WeaponFiring_POST(int ID, UObject *dwCallingObject, UFunction* pFunction, void* pParams, void* pResult)
 {
 	if (fired_proj)
 	{
--- TAMods/Mods.h
+++ TAMods/Mods.h
@@ -48,8 +48,8 @@ bool TrPC_InitInputSystem(int ID, UObject *dwCallingObject, UFunction* pFunction
 bool TrPC_ClientMatchOver(int ID, UObject *dwCallingObject, UFunction* pFunction, void* pParams, void* pResult);
 
 // Player only bullet color
-bool TrDev_ConstFire_WeaponConstantFiring(int ID, UObject *dwCallingObject, UFunction* pFunction, void* pParams, void* pResult);
-bool TrDev_ConstFire_WeaponConstantFiring_POST(int ID, UObject *dwCallingObject, UFunction* pFunction, void* pParams, void* pResult);
+bool TrDev_WeaponFiring(int ID, UObject *dwCallingObject, UFunction* pFunction, void* pParams, void* pResult);
+bool TrDev_WeaponFiring_POST(int ID, UObject *dwCallingObject, UFunction* pFunction, void* pParams, void* pResult);
 
 // Lua console command
 bool TrChatConsole_InputKey(int id, UObject *dwCallingObject, UFunction* pFunction, void* pParams, void* pResult);
