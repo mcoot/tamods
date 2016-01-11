c124de20c6dcc50029c6de932ead57a26b06ff62
Ensis <ensis.tribes@gmail.com>
2016-01-11 10:29:18 +0100

    Update items data

--- TAMods/Data.h
+++ TAMods/Data.h
@@ -7,14 +7,13 @@
 
 namespace Data
 {
-	extern std::map<std::string, int> equipment[9][4];
+	extern std::map<std::string, int> weapons[3][7];
 	extern std::map<int, std::string> weapon_id_to_name;
 	extern std::map<int, std::string> weapon_id_to_proj_name;
-	extern std::map<std::string, int> perks[2];
 	extern std::map<std::string, int> vehicle_weapons;
 	extern std::map<std::string, int> classes;
 	extern std::map<std::string, int> crosshairs;
-	extern int classes_id[9];
+	extern int classes_id[3];
 	extern std::map<int, int> class_id_to_nb;
 	extern std::string class_type_name[];
 	extern std::string equip_type_name[];
