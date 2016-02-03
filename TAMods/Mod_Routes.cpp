f8a80cf61776e2b3d4829cb6462d0ab3e89b0a8f
Schreq <mastaag@gmail.com>
2016-02-03 13:26:20 +0100

    Mod version should be 0.5, not 5.0

--- TAMods/Mod_Routes.cpp
+++ TAMods/Mod_Routes.cpp
@@ -531,7 +531,7 @@ static void reloadRouteList()
 			routefile >> file_map_name;
 			routefile.close();
 
-			if (mod_version >= 5.0f && file_map_name == curr_map_name)
+			if (mod_version >= 0.5f && file_map_name == curr_map_name)
 				files.push_back(fname);
 		}
 
@@ -678,7 +678,7 @@ void routeLoadFile(unsigned int num)
 		routeReset();
 		binary_read(routefile, modVersion);
 
-		if (modVersion < 5.0f)
+		if (modVersion < 0.5f)
 		{
 			routefile.close();
 			Utils::console("Error: This route file has a wrong format");
