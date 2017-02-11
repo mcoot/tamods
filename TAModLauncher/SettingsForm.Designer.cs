87a8e36bee456b0c463ceb57afacea7903637536
Todd Butler <ToddButler93@outlook.com.au>
2017-02-11 12:35:14 +1100

    Stupid sexy flanders

--- TAModLauncher/SettingsForm.Designer.cs
+++ TAModLauncher/SettingsForm.Designer.cs
@@ -74,9 +74,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 50);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.Size = new System.Drawing.Size(117, 13);
             this.label2.TabIndex = 13;
-            this.label2.Text = "Tribes.exe Path";
+            this.label2.Text = "TribesAscend.exe Path";
             // 
             // fileSelectLauncherDirectory
             // 
