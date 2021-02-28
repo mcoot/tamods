db2efe9158362871080676c7c10386151ba3f9e1
Maurice van der Pot <griffon26@kfk4ever.com>
2021-02-28 16:26:28 +0100

    Fix some incorrect map IDs

--- TAMods/Data.h
+++ TAMods/Data.h
@@ -14,6 +14,7 @@
 #define CONST_MAP_ID_RABBIT_QUICKSAND           1461
 #define CONST_MAP_ID_CTF_CROSSFIRE              1462
 #define CONST_MAP_ID_RABBIT_CROSSFIRE           1464
+#define CONST_MAP_ID_CTF_BELLAOMEGA             1473
 #define CONST_MAP_ID_TDM_DRYDOCKNIGHT           1480
 #define CONST_MAP_ID_TDM_CROSSFIRE              1482
 #define CONST_MAP_ID_TDM_QUICKSAND              1484
@@ -34,15 +35,18 @@
 #define CONST_MAP_ID_CAH_KATABATIC              1521
 #define CONST_MAP_ID_CTF_STONEHENGE             1522
 #define CONST_MAP_ID_CTF_SUNSTAR                1523
+#define CONST_MAP_ID_CAH_DRYDOCKNIGHT           1525
 #define CONST_MAP_ID_CAH_OUTSKIRTS3P            1526
 #define CONST_MAP_ID_CAH_RAINDANCE              1528
-#define CONST_MAP_ID_CAH_DRYDOCKNIGHT           1530
 #define CONST_MAP_ID_ARENA_HINTERLANDS          1533
 #define CONST_MAP_ID_CTF_PERMAFROST             1534
 #define CONST_MAP_ID_CAH_SULFURCOVE             1535
 #define CONST_MAP_ID_TDM_MIASMA                 1536
 #define CONST_MAP_ID_CAH_TARTARUS               1537
 #define CONST_MAP_ID_CTF_DANGEROUSCROSSING      1538
+#define CONST_MAP_ID_BLITZ_KATABATIC            1539
+#define CONST_MAP_ID_BLITZ_ARXNOVENA            1540
+#define CONST_MAP_ID_BLITZ_DRYDOCK              1541
 #define CONST_MAP_ID_BLITZ_CROSSFIRE            1542
 #define CONST_MAP_ID_CTF_BLUESHIFT              1543
 #define CONST_MAP_ID_ARENA_WHITEOUT             1544
@@ -54,17 +58,13 @@
 #define CONST_MAP_ID_CTF_BELLAOMEGANS           1551
 #define CONST_MAP_ID_BLITZ_BLUESHIFT            1552
 #define CONST_MAP_ID_CTF_TERMINUS               1553
-#define CONST_MAP_ID_BLITZ_TERMINUS             1559
 #define CONST_MAP_ID_CTF_ICECOASTER             1554
-#define CONST_MAP_ID_BLITZ_ICECOASTER           1558
 #define CONST_MAP_ID_CTF_PERDITION              1555
 #define CONST_MAP_ID_TDM_PERDITION              1557
+#define CONST_MAP_ID_BLITZ_ICECOASTER           1558
+#define CONST_MAP_ID_BLITZ_TERMINUS             1559
 #define CONST_MAP_ID_CTF_HELLFIRE               1560
 #define CONST_MAP_ID_BLITZ_HELLFIRE             1561
-#define CONST_MAP_ID_CTF_BELLAOMEGA             1573
-#define CONST_MAP_ID_BLITZ_ARXNOVENA            2566
-#define CONST_MAP_ID_BLITZ_KATABATIC            2598
-#define CONST_MAP_ID_BLITZ_DRYDOCK              2599
 
 namespace Data
 {
